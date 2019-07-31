378ee198946b11f839d84249e9a5f25d175bc46e
Gregory Desmarais <DesmaraisG@lumosnet.com>
2019-07-31 14:41:54 -0400

    Started to Javascript.  Created wordGame Object

---
+++ code.js
@@ -0,0 +1,21 @@
+var wordGuess = {
+    tries: 15,
+    availWords: ["javascript", "jquery", "CSS", "HTML", "bootstrap", "react"],
+    curWord: "",
+    usedWords: [],
+    userWins: 0,
+    guessedLetters: [],
+    newGame: function () {
+        var select = Math.ceil(Math.random() * wordGuess.availWords.length);
+        wordGuess.curWord = wordGuess.availWords[select];
+        console.log(wordGuess.curWord);
+        if (wordGuess.usedWords.indexOf(wordGuess.curWord) > -1) {
+            wordGuess.usedWords.push(wordGuess.curWord);
+            console.log(wordGuess.curWord)
+        }
+        else{
+            console.log("Word already exists");
+            //wordGuess.newGame();
+        }
+    }
+}
