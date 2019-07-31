1baca7387fd4e8d290599c09b991fd17f8f45d85
GDesmarais <46517644+GregoryDesmarais@users.noreply.github.com>
2019-07-31 18:36:28 -0400

    Adding additional functions for wordGuess

--- code.js
+++ code.js
@@ -5,17 +5,39 @@ var wordGuess = {
     usedWords: [],
     userWins: 0,
     guessedLetters: [],
-    newGame: function () {
-        var select = Math.ceil(Math.random() * wordGuess.availWords.length);
+    test: function(letter){
+        if(this.curWord.length > 0)
+        {
+            wordGuess.playerGuess(letter);
+        }
+        else 
+        {
+            wordGuess.newGame();
+        }
+    },
+    newGame: function() {
+        var select = Math.floor(Math.random() * wordGuess.availWords.length);
         wordGuess.curWord = wordGuess.availWords[select];
         console.log("Selected word: " + wordGuess.curWord);
         if (wordGuess.usedWords.indexOf(wordGuess.curWord) > -1) {
-            wordGuess.usedWords.push(wordGuess.curWord);
-            console.log(wordGuess.curWord)
+            console.log("Word already exists");
+            if (wordGuess.usedWords.length == wordGuess.availWords.length) {
+                alert("All words solved!\n\nGreat Game!");
+            }
+            else {
+                wordGuess.newGame();
+            }
         }
         else {
-            console.log("Word already exists");
-            //wordGuess.newGame();
+            console.log(wordGuess.curWord)
+            wordGuess.usedWords.push(wordGuess.curWord);
         }
-    }
+    },
+
+}
+
+document.onkeyup = function(e) {
+    wordGuess.test(e.key);
+    //alert(e.key);
+
 }
