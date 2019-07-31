366ffbb831a89de54008b72ae48037c6a71c8113
Gregory Desmarais <DesmaraisG@lumosnet.com>
2019-07-31 14:43:36 -0400

    Forgot to save code.js

--- code.js
+++ code.js
@@ -8,12 +8,12 @@ var wordGuess = {
     newGame: function () {
         var select = Math.ceil(Math.random() * wordGuess.availWords.length);
         wordGuess.curWord = wordGuess.availWords[select];
-        console.log(wordGuess.curWord);
+        console.log("Selected word: " + wordGuess.curWord);
         if (wordGuess.usedWords.indexOf(wordGuess.curWord) > -1) {
             wordGuess.usedWords.push(wordGuess.curWord);
             console.log(wordGuess.curWord)
         }
-        else{
+        else {
             console.log("Word already exists");
             //wordGuess.newGame();
         }
