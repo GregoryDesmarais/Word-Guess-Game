a0b180c2f40cf6a9226b9b7578f30e31e1aaf042
Greg D <gregorydesmarais@gmail.com>
2019-08-02 12:07:38 -0400

    Changed newGame function to pickWord.  Added new newGame function. added displayWord function.  Added curWord field to html.  Added wordGuess.curWordStatus to keep track of the positions of the active word.

--- code.js
+++ code.js
@@ -2,42 +2,65 @@ var wordGuess = {
     tries: 15,
     availWords: ["javascript", "jquery", "CSS", "HTML", "bootstrap", "react"],
     curWord: "",
+    curWordStatus: [],
     usedWords: [],
     userWins: 0,
     guessedLetters: [],
-    test: function(letter){
-        if(this.curWord.length > 0)
-        {
-            wordGuess.playerGuess(letter);
+    letterGuess: function(letter) {
+        if (this.curWord.length > 0) {
+            this.playerGuess(letter);
+        } else {
+            //wordGuess.newGame();
         }
-        else 
-        {
-            wordGuess.newGame();
+    },
+    pickWord: function() {
+        var select = Math.floor(Math.random() * this.availWords.length);
+        this.curWord = this.availWords[select];
+        console.log("Selected word: " + this.curWord);
+        if (this.usedWords.indexOf(this.curWord) > -1) {
+            console.log("Word already exists");
+            if (this.usedWords.length === this.availWords.length) {
+                alert("All words solved!\n\nGreat Game!");
+                //wordGuess.newGame();
+            } else {
+                this.pickWord();
+            }
+        } else {
+            console.log(this.curWord)
+            this.usedWords.push(this.curWord);
+            this.curWordStatus = [];
+            for (var i = 0; i < this.curWord.length; i++) {
+                this.curWordStatus.push("_");
+            }
         }
     },
     newGame: function() {
-        var select = Math.floor(Math.random() * wordGuess.availWords.length);
-        wordGuess.curWord = wordGuess.availWords[select];
-        console.log("Selected word: " + wordGuess.curWord);
-        if (wordGuess.usedWords.indexOf(wordGuess.curWord) > -1) {
-            console.log("Word already exists");
-            if (wordGuess.usedWords.length == wordGuess.availWords.length) {
-                alert("All words solved!\n\nGreat Game!");
-            }
-            else {
-                wordGuess.newGame();
-            }
-        }
-        else {
-            console.log(wordGuess.curWord)
-            wordGuess.usedWords.push(wordGuess.curWord);
-        }
+        this.tries = 15;
+        this.curWord = "";
+        this.curWordStatus = [];
+        this.usedWords = [];
+        this.userWins = 0;
+        this.guessedLetters = 0;
+        this.pickWord();
     },
+    displayWord: function() {
+        var word = "";
+        for (var i = 0; i < this.curWordStatus.length; i++) {
+            word += this.curWordStatus[i];
+            if (i === this.curWordStatus.length - 1) {
+                continue;
+            } else {
+                word += " ";
+            }
+        }
+        document.getElementById("curWord").textContent = word;
+    }
+
 
 }
 
 document.onkeyup = function(e) {
-    wordGuess.test(e.key);
+    wordGuess.letterGuess(e.key);
     //alert(e.key);
 
 }
